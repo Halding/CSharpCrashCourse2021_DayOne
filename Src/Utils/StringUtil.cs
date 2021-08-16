a4e9a9391b4cdf5cbea6373c6d2b8c4e01f5c5a4
Halding <89019283+Halding@users.noreply.github.com>
2021-08-16 15:16:00 +0200

    Update StringUtil.cs

--- Src/Utils/StringUtil.cs
+++ Src/Utils/StringUtil.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CrashCourse2021ExercisesDayOne.Utils
 {
@@ -10,37 +11,84 @@ namespace CrashCourse2021ExercisesDayOne.Utils
             {
                 throw new NullReferenceException(Constants.StringCannotBeNull);
             }
+            else
+            {
+                return stringToMeasure.Length;
+            }
 
             throw new NotImplementedException();
         }
 
         internal string SumStrings(string value1, string value2)
         {
+            int sum = Int32.Parse(value1) + Int32.Parse(value2);
+
+            return sum.ToString();
+            
             throw new NotImplementedException();
         }
 
         internal string DivideString(string value1, string value2)
         {
+            
+            int sum = Int32.Parse(value1) / Int32.Parse(value2);
+
+            return sum.ToString();
+            
             throw new NotImplementedException();
         }
 
         internal string StringContains(string value1, string value2)
         {
+            var yes = "YES";
+            var no = "NO";    
+            
+            if (value1.Contains(value2))
+            {
+                return yes;
+            }
+            else
+            {
+                return no;
+            }
+            
             throw new NotImplementedException();
         }
 
         internal string StringToUpperCase(string value1)
         {
+            return value1.ToUpper();
+            
             throw new NotImplementedException();
         }
 
         internal string AdditionFromPlusString(string inputString)
         {
+            string[] talList = inputString.Split('+');
+
+            int sum = 0;
+
+            foreach (var tal in talList)
+            {
+                sum += Int32.Parse(tal);
+            }
+
+            return sum.ToString();
+            
             throw new NotImplementedException();
         }
 
         internal string EvenNumber(int numbertoTest)
         {
+            if (numbertoTest % 2 == 0)
+            {
+                return Utils.Constants.Even;
+            }
+            else
+            {
+                return Utils.Constants.Odd;
+            }
+            
             throw new NotImplementedException();
         }
     }
